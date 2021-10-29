00d822f34ee5974d90a8a055b532cda4a4f8b211
p0tkun <itzmaddylolz23@gmail.com>
2021-10-29 10:26:16 +0000

    fourthcommit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,14 @@
 <script>
     import Header from "$lib/Header.svelte";
+    import Section from "$lib/Section.svelte";
+
+    let myInfo = {
+    title: "Info",
+    item1: "Tan Mei Wen",
+    item2: 14,
+    item3: "WMSPI",
+    }
+
 </script>
 <Header title = "My Biography" />
+<Section {...myInfo} />
