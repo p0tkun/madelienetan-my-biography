6a9371683b0151f030b90d7b66596bb67764c099
p0tkun <itzmaddylolz23@gmail.com>
2021-10-29 09:51:29 +0000

    secondcommit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import vercel from '@sveltejs/adapter-vercel';
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
    	 // hydrate the <div id="svelte"> element in src/app.html
