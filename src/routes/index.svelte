bfdbee48b595cb5b8650498ffa0e8ce120682ffc
p0tkun <itzmaddylolz23@gmail.com>
2021-10-29 10:00:17 +0000

    secondcommit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,2 @@
-<script>
-    import Header from "$lib/Header.svelte";
-</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
